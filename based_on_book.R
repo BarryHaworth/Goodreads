53d6433bc31ad6482927afb40c43fa71ed596a99
Barry Haworth <barryhaworth@gmail.com>
2021-08-24 08:57:13 +1000

    Web Rip to find movie keys

--- based_on_book.R
+++ based_on_book.R
@@ -11,17 +11,57 @@ DATA_DIR    <- paste0(PROJECT_DIR,"/data")
 
 # Read the data
 load(file=paste0(DATA_DIR,"/movie_list.RData"))
-sum(movie_list$movie_votes > 100000,na.rm=T)  # Movies with > 100K votes
 
-movies_10K <- movie_list %>% filter(movie_votes > 100000) %>% select("tconst")
+movies <- movie_list %>% 
+  filter(movie_votes> 1000) %>%
+  select("tconst","movie_votes","primaryTitle") %>%
+  arrange(-movie_votes) %>%
+  unique()
 
-# Rip the keywords page
-tconst <- movies_10K$tconst[1]
+# Rip the keywords page - test
+tconst <- movies$tconst[1]
 tconst <- "tt0120855"  # Disney Tarzan
 tconst <- "tt0304141"  # Harry Potter
-tconst <- "tt5687612"
+tconst <- "tt0073486"
 url <- paste0('https://www.imdb.com/title/',tconst,'/keywords')
 webpage <- read_html(url)
 tag_html <- html_nodes(webpage,'.sodatext')
 tags <- trimws(gsub('[\n]', '', html_text(tag_html)))
+keywords <- data.frame("tconst"=tconst,"keywords"=tags)
 tags[grep("based on",tags)]
+length(grep("based on",tags))
+
+# create a keywords dataframe
+movie_keys <- function(id){
+  url <- paste0('https://www.imdb.com/title/',id,'/keywords')
+  webpage <- read_html(url)
+  tag_html <- html_nodes(webpage,'.sodatext')
+  tags <- trimws(gsub('[\n]', '', html_text(tag_html)))
+  keywords <- data.frame("tconst"=id,"keywords"=tags)
+  return(keywords)
+}
+
+# If the saved Movie keywords data exists, read it.
+if (file.exists(paste0(DATA_DIR,"/movie_keywords.RData"))) {
+  load(paste0(DATA_DIR,"/movie_keywords.RData"))
+}
+  
+# If the local file does not exist, initialise it
+if (!exists("movie_keywords")) {
+  movie_keywords <- keywords(movies_10K$tconst[1])  # Initialise the keywords data frame
+}
+
+# Get the list of IDs looked up
+
+looked_up <- movie_keywords$tconst %>% unique()
+
+movies_notyet <- movies %>% filter(!(tconst %in% looked_up))
+
+for (i in 1:1000){
+  print(paste("Looking up keywords for movie",i,movies_notyet$primaryTitle[i]))
+  keys <- movie_keys(movies_notyet$tconst[i])
+  if (length(grep("based on",keys))>0) print("                      - Based on a book (or something)")
+  movie_keywords <- rbind(movie_keywords,keys)
+  }
+
+save(movie_keywords,file=paste0(DATA_DIR,"/movie_keywords.RData"))
